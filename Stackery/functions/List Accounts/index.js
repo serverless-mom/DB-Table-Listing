6ae5e0334ac3fb802b1d2231c010bdda7ca14c9f
Toby Fee <TobyFee@gmail.com>
2018-07-25 18:34:10 -0700

    cleanup

--- Stackery/functions/List Accounts/index.js
+++ Stackery/functions/List Accounts/index.js
@@ -1,7 +1,5 @@
 const knex = require('knex');
 
-const responseBody = require('./responseBody')
-
 const connectionName = process.env.CONNECTION || 'development';
 const connection = require('knexfile')[connectionName];
 
@@ -37,25 +35,22 @@ module.exports = async message => {
     }
 
     let htmlTable = `<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>DynamoDB table - ${table}</title>
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-  </head>
-  <body>  
-    <table class='table table-striped'>
-    <tr>${header}</tr>
-    ${rows.join('')}
-</table>
-</body>
-</html>`
+    <html lang='en'>
+      <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>DynamoDB table - ${table}</title>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+      </head>
+      <body>  
+        <table class='table table-striped'>
+        <tr>${header}</tr>
+        ${rows.join('')}
+    </table>
+    </body>
+    </html>`
 
-
-
-    
     
   // Build an HTTP response.
   let response = {
