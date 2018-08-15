c7bcf7801be0558c4e1691b0162daed1fa4360d3
Toby Fee <TobyFee@gmail.com>
2018-08-15 16:53:12 -0700

    adding a catch block

--- Stackery/functions/List Accounts/index.js
+++ Stackery/functions/List Accounts/index.js
@@ -3,15 +3,15 @@ const knex = require('knex');
 const connectionName = process.env.CONNECTION || 'development';
 const connection = require('knexfile')[connectionName];
 
-var iopipe = require('@iopipe/iopipe')({
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNTIzMmYwZi0yYmIxLTQzZTEtOTljZC1kMmJhNDc2ZWJmOTEiLCJqdGkiOiJjZGVmMmQ3NC1iOGYzLTQzYzgtOTBkNS0yMWNlNzg2NTFhM2QiLCJpYXQiOjE1MzMwNjEzOTMsImlzcyI6Imh0dHBzOi8vaW9waXBlLmNvbSIsImF1ZCI6Imh0dHBzOi8vaW9waXBlLmNvbSxodHRwczovL21ldHJpY3MtYXBpLmlvcGlwZS5jb20vZXZlbnQvLGh0dHBzOi8vZ3JhcGhxbC5pb3BpcGUuY29tIn0.UvWdEuZA8PI2eZmIiJ9YzpkUtzooCR0v1P9iTp61Gas'
-});
+
+
+const rookout = require('rookout/lambda');
 
 /**
  * Fetch list of accounts from database and respond with an array of account
  * names.
  */
-module.exports = iopipe(async message => {
+module.exports = rookout.wrap(async message => {
   const client = knex(connection);
   console.dir(message)
   let table = message.pathParameters.table
@@ -61,9 +61,14 @@ module.exports = iopipe(async message => {
       "Content-Type": "text/html"
     },
     body: htmlTable
-  };
+  } 
 
   return response;
+  }catch {
+   console.log('hitting the catch block') ;
+   return response = {};
+   return response;
+
   } finally {
     client.destroy();
   }
